b6a50afca016d213ffd574127a57003f06cd80e9
gudtjpq <hyeongseo726@gmail.com>
2025-03-14 09:42:20 +0900

    Add files via upload
    Signed-off-by: gudtjpq <hyeongseo726@gmail.com>

---
+++ student-cafteria-ticket-vending/student_cafeteria/src/test/java/com/fouridiots/student_cafeteria/StudentCafeteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fouridiots.student_cafeteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCafeteriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
